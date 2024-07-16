d82d44b8748124d2dc06b0fee78178730d4c9351
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-16 01:25:09 +0100

    Created read page with responsiveness: concluding read page

---
+++ src/components/chapLinks.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function ChapLink() {
+  return (
+    <div className='flex flex-col'>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 1</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 2</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 3</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 4</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 5</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 6</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 7</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 8</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 9</Link>
+
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 8</Link>
+        <Link className='w-full sm:h-[120px] active:scale-[1.2] active:bg-[--accent1] active:text-[--bg] h-[90px] flex items-center justify-center sm:hover:scale-110 duration-[0.1s] sm:duration-[0.5s] sm:hover:text-white sm:hover:bg-[--accent1] bg-[--bg] py-[2em] border-2 border-[--accent1] text-xl sm:text-2xl text-center orb font-bold text-[--accent1]'>Chapter 9</Link>
+    </div>
+  )
+}
+
+export default ChapLink
