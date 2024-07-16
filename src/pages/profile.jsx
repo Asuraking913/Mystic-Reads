cecbcdc4393001b141856bf6428cdb7bbb7344ad
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-16 02:07:16 +0100

    Completed reading page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -143,7 +143,7 @@ function Profile() {
 
       <article className='h-auto sm:mt-0 mt-[3.7em]'>
         {error && <p className='sm:text-2xl rounded-[1em] animate-bounce text-white roboto absolute z-[1000] bg-[--accent1] sm:p-[.5em] p-[.2em] px-[1em] left-[38%] sm:left-[45%] top-[5em] sm:top-[4em]'>{error}</p>}
-        <div className='sm:h-[50vh] h-[30vh] relative w-full linear'>
+        <div className='sm:h-[50vh] h-[20vh] relative w-full linear'>
           {
             cover === null ? "" :
             <Link onClick={handleCoverDel} className='text-[1rem] bg-[--accent] p-[.3em] active:bg-white text-[white] rounded-[2px] absolute right-[1em] sm:top-[5%] top-[.5em] bottom hover:text-[--accent] sm:active:scale-[1]  hover:scale-110 duration-[0.5s]'>
@@ -154,7 +154,7 @@ function Profile() {
             </span>
           </Link>
           }
-          <Link onClick={handleCoverBtn} className='absolute bg-[--accent] p-[.2em] rounded-[2px] text-white active:scale-[0.95] right-[1em] flex gap-[.2em] roboto font-bold justifsm:y-center items-center hover:scale-110 duration-[0.5s] top-[85%]'>
+          <Link onClick={handleCoverBtn} className='absolute bg-[--accent] p-[.2em] rounded-[2px] text-white active:scale-[0.95] right-[1em] flex gap-[.2em] roboto font-bold justifsm:y-center items-center hover:scale-110 duration-[0.5s] top-[80%] sm:top-[90%]'>
             <img src={edit} className='w-[20px]' alt="" />
             {/* <FontAwesomeIcon  icon={faPlusCircle}/> */}
             {/* Change Photo */}
@@ -227,7 +227,7 @@ function Profile() {
                     </span>
                   </Link>
                   }
-                  <img src={profile} className=' rounded-[50%] shadow-md shadow-[black] w-full h-full object-cover' alt="" />
+                  <img src={profile} className='rounded-[50%] shadow-md shadow-[black] w-full h-full object-cover' alt="" />
                   <img src={update} onClick={handleProfileBtn} className='absolute active:scale-[0.95] w-[20px] cursor-pointer top-[80%] right-[1em] hover:scale-125 duration-[0.5s] ' alt="" />
                   <input type="file" className='hidden' onChange={handleProfile} ref={profileInput} name="file" id="profile" />
                </div>
@@ -273,9 +273,9 @@ function Profile() {
         </div>
         <div className='sm:h-[60px] h-[40px] items-center justify-center px-[4em] flex sticky top-[3.5em] z-[10] bg-[--accent1]'>
           <ul className='flex sm:ml-[8em] sm:justify-normal justify-between sm:gap-[10em] sm:w-auto w-full'>
-              <Link className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.5s] py-[.1em] sm:px-[.5em]'><img src={posts} title='Posts' className='sm:w-[40px] w-[30px] sm:h-[50px]' alt="" /></Link>
-              <Link   className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.5s] py-[.1em] sm:px-[.5em]'><img src={book}  title='Puslished' className='sm:w-[40px] w-[30px] sm:h-[50px]'alt="" /></Link>
-              <Link className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.5s] py-[.1em] sm:px-[.5em]'><img src={likes} title='Likes' className='sm:w-[40px] w-[30px] sm:h-[50px]' alt="" /></Link>
+              <Link className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.1s] py-[.1em] sm:px-[.5em]'><img src={posts} title='Posts' className='sm:w-[40px] w-[30px] sm:h-[50px]' alt="" /></Link>
+              <Link   className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.1s] py-[.1em] sm:px-[.5em]'><img src={book}  title='Puslished' className='sm:w-[40px] w-[30px] sm:h-[50px]'alt="" /></Link>
+              <Link className='active:bg-[--accent] sm:hover:bg-[--accent] sm:py-0 active:scale-[0.95] hover:scale-110 rounded-[5px] duration-[0.1s] py-[.1em] sm:px-[.5em]'><img src={likes} title='Likes' className='sm:w-[40px] w-[30px] sm:h-[50px]' alt="" /></Link>
             </ul>
       
         </div>
--- src/pages/read.jsx
+++ src/pages/read.jsx
@@ -3,7 +3,7 @@ import Nav from '../components/nav'
 import { Link } from 'react-router-dom'
 import next from "../assets/next.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBackward, faBookOpen, faCross, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBookOpen, faCircleArrowDown, faCircleArrowUp } from '@fortawesome/free-solid-svg-icons'
 import ChapLink from '../components/chapLinks'
 
 function ReadPage() {
@@ -11,17 +11,34 @@ function ReadPage() {
 
   const handlechap = () => {
     setChap(!chap)
+    document.body.classList.add('no-scroll');
   };
 
   const exitChap = (event) => {
+    document.body.classList.remove('no-scroll');
     if (event.target.id == "exit") {
       setChap(!chap)
     }
   }
 
+  const scrollDown = () => {
+    document.getElementById('down').scrollIntoView({behavior : 'smooth'})
+  }
+
+  const scrollUp = () => {
+    document.getElementById('up').scrollIntoView({behavior : 'smooth'})
+  }
+
+
   return (
     <>
         <Nav />
+        <Link onClick={scrollDown}>
+          <FontAwesomeIcon icon={faCircleArrowDown} className='text-[--accent1] animate-pulse sm:hidden right-[10px] fixed top-[30%] text-3xl'/>
+        </Link>
+        <Link onClick={scrollUp}>
+          <FontAwesomeIcon icon={faCircleArrowUp} className='text-[--accent1] animate-pulse sm:hidden right-[10px] fixed bottom-[20%] text-3xl'/>
+        </Link>
         {
           !chap ? "" : 
           <div id='exit' onClick={exitChap} className='h-screen w-full z-[20000000000000] flex sm:justify-center fixed top-0 bg-[#000000de]'>
@@ -32,10 +49,10 @@ function ReadPage() {
           </div>
         }
         
-        <section className='sm:px-[--pdx] px-[.5em] mt-[3em] py-[2em] flex flex-col justify-center items-center'>
-          <div className='flex gap-[1em] flex-col w-full items-center'>
+        <section className='sm:px-[--pdx] px-[.5em] mt-[3em] py-[2em] flex flex-col justify-center items-center' id='up'>
+          <div className='flex gap-[1em] flex-col w-full items-center' >
             <div className='text-center'>
-              <h1 className='sm:text-2xl text-xl roboto font-bold text-[--accent]'>Doluo Dalu 3: Soul Land 3</h1>
+              <h1 className='sm:text-2xl text-xl roboto font-bold text-[--accent1] text1'>Doluo Dalu 3: Soul Land 3</h1>
               <h2 className='orb sm:text-[1rem] text-[0.8rem]'>Chapter 234: The legend Of the golden Dragon King</h2>
             </div>
             <div className='flex gap-[.5em] items-center  justify-center'>
@@ -43,7 +60,7 @@ function ReadPage() {
                 <img src={next} className='sm:w-[30px] h-[20px] w-[10px] object-cover rotate-180' alt="" />
                 <p className=''>Prev Chapter</p>
               </Link>
-              <button onClick={handlechap} title='Chapters' className='s,:h-[40px] hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s] sm:w-[40px] w-[25px] h-[25px] flex items-center justify-center bg-[--accent1] rounded-[1em]'>
+              <button onClick={handlechap} title='Chapters' className='sm:h-[40px] hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s] sm:w-[40px] w-[25px] h-[25px] flex items-center justify-center bg-[--accent1] rounded-[10px] sm:rounded-[1em]'>
                   <FontAwesomeIcon icon={faBookOpen} className='w-[70%]  h-[70%] text-white'/>
                 </button>
               <Link className='py-[.5em] flex items-center gap-[.5em] px-[1em] hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s]  bg-[--accent1] rounded-[2em] text-[--wh] roboto text-[0.8rem] sm:text-xl'>
@@ -54,18 +71,18 @@ function ReadPage() {
           </div>
           <p className='sm:py-[2em] py-[1em] font-sans sm:text-[1.1rem] line1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident vero saepe error vel tempore cumque eum, dignissimos nam quisquam eligendi ea omnis alias dolor unde cum? Vel omnis, sunt incidunt at obcaecati velit itaque impedit hic maiores illo aliquid fugiat est minus alias facilis soluta et nulla odit adipisci, eos natus! Inventore iste animi sequi perferendis error voluptate, harum, delectus recusandae sed ea quidem dignissimos voluptatibus vitae possimus, ab esse. Cum deserunt expedita atque, exercitationem itaque vel odit! Officiis enim ex accusantium quo. Possimus laborum, repellat commodi animi, dolore quam odio dolor enim, alias voluptatum aliquam ipsam impedit quidem. Ut in exercitationem dicta sapiente non iure nostrum illum nulla corrupti voluptas dolore repudiandae nisi porro deserunt, iste veniam accusamus blanditiis. Laudantium reiciendis accusamus corrupti vitae, labore itaque commodi ipsam enim maiores saepe in eos modi fugiat, sed est blanditiis asperiores. Corporis reiciendis, quibusdam quam ducimus quo atque et ab quas quod placeat totam, numquam consequatur distinctio ut maxime adipisci quos ullam illo fuga. Molestiae eius quisquam ipsum a dolor, ad quod aliquid tempore expedita. Aliquam beatae debitis, tempora voluptatum eaque rerum quibusdam sunt velit saepe quod dignissimos consectetur quisquam fugiat ipsum, eveniet neque quia. Libero suscipit labore provident facere. Incidunt pariatur vitae debitis quod excepturi id sapiente libero. Omnis tempore vero culpa corporis aut nobis commodi maiores illo ut iste, explicabo molestiae alias consequuntur repudiandae obcaecati architecto. Ratione, sapiente quibusdam rerum illo repellendus quaerat voluptatum sed eum ut nisi debitis tenetur, adipisci atque quia! Numquam veniam, optio facilis dignissimos molestiae facere neque quaerat, hic deserunt praesentium odit totam. Atque molestiae error officia dolorem totam, aspernatur, illo maiores ipsa iure incidunt harum doloremque? Numquam assumenda neque iusto eveniet, accusamus repellendus quidem perspiciatis, soluta quisquam maxime eum. Commodi et unde adipisci, modi nesciunt fugit cupiditate ut molestiae, dolorem quas voluptas ad eius exercitationem possimus laudantium maxime quaerat aliquid! Odio corrupti itaque quibusdam eaque quae facere vel asperiores maiores quos laudantium quod nesciunt totam libero perspiciatis, sapiente dicta non harum vitae expedita consequatur commodi explicabo tempore. Consequuntur iusto libero modi soluta sunt. Laboriosam eos omnis enim provident suscipit quod praesentium magni fugiat repudiandae reprehenderit repellat nulla dolorem natus dicta consectetur, voluptatibus consequatur exercitationem nemo aut culpa veniam iusto! Perferendis asperiores excepturi ex iste earum deserunt fugiat soluta, animi corporis minima veritatis impedit laudantium rem enim omnis possimus eaque optio corrupti voluptatem accusantium, quaerat quisquam, aperiam veniam quo. A illum ipsam veniam. Dignissimos id quos temporibus illo quis ut, nam voluptatibus fugiat! Magni rem similique error delectus molestiae consectetur dolores necessitatibus explicabo optio consequuntur, corporis quos quaerat eaque quia, laborum aliquam nemo dignissimos tempora. Eius necessitatibus reiciendis amet omnis totam obcaecati, veritatis quasi nemo velit id nesciunt inventore officia, fugiat nobis! Blanditiis perferendis impedit mollitia, iure consequatur libero cum ea ducimus doloribus nulla earum, quam, incidunt ipsum voluptatum aspernatur dicta sit reiciendis voluptate. Vitae similique placeat molestiae modi ipsum harum consequuntur, facere quia aliquam id illum nostrum pariatur veniam recusandae qui error reprehenderit dolore eum in vero nulla quae! Facere, optio labore soluta atque vitae consequatur similique accusantium! Voluptates, necessitatibus molestias qui placeat vitae dicta aut minima quas mollitia autem error dolorum architecto esse, maxime quaerat est laudantium eum doloremque numquam, quod ratione aliquam sunt quisquam ducimus! Harum vel corrupti blanditiis porro officia. Ab reiciendis cupiditate molestiae quos pariatur, quo consequuntur praesentium voluptatibus error beatae quas nemo harum voluptas ipsum tenetur! Repellat aperiam dignissimos facere blanditiis adipisci doloremque sapiente? Voluptate aut eos veritatis ipsum dolores aperiam dolor consequatur iusto reprehenderit atque, id nemo voluptatibus nostrum eaque vel, hic explicabo totam sint, tempore vero magni. Aperiam cupiditate, distinctio optio molestiae libero doloremque omnis? Pariatur, libero.</p>
         </section>
-        <div className=' flex flex-col gap-[1em] mb-[2em]'>
+        <div className=' flex flex-col gap-[1em] mb-[2em]' id='down'>
               <div className='flex gap-[.5em] items-center  justify-center'>
                 <Link className='py-[.5em] flex items-center gap-[.5em] px-[1em] sm:hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s]  bg-[--accent1] rounded-[2em] text-[--wh] roboto text-[0.8rem] sm:text-xl'>
                   <img src={next} className='sm:w-[30px] h-[20px] w-[10px] object-cover rotate-180' alt="" />
                   <p className=''>Prev Chapter</p>
                 </Link>
-                <Link onClick={handlechap} title='Chapters' className='h-[40px] sm:hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s] w-[40px] flex items-center justify-center bg-[--accent1] rounded-[1em]'>
-                  <FontAwesomeIcon icon={faBookOpen} className='w-[70%]  h-[70%] text-white'/>
+                <Link onClick={handlechap} title='Chapters' className='sm:h-[40px] hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s] sm:w-[40px] w-[25px] h-[25px] flex items-center justify-center bg-[--accent1] rounded-[10px] sm:rounded-[1em]'>
+                  <FontAwesomeIcon icon={faBookOpen} className='sm:w-[70%]  sm:h-[70%] text-white'/>
                 </Link>
                 <Link className='py-[.5em] flex items-center gap-[.5em] px-[1em] sm:hover:scale-105 active:scale-[0.8] sm:active:scale-[1] duration-[0.5s]  bg-[--accent1] rounded-[2em] text-[--wh] roboto text-[0.8rem] sm:text-xl'>
                   <p>Next Chapter</p>
-                  <img src={next} className='sm:w-[30px] h-[20px] w-[10px] object-cover' alt="" />
+                  <img src={next} className='sm:w-[30px]  h-[20px] w-[10px] object-cover' alt="" />
                 </Link>
               </div>
               <div className='flex justify-center'>
